a3fd38e6c80bf9f99bc8777920c78229727e59da
Antwan Foster <Antwan3000@gmail.com>
2011-01-22 17:15:59 -0500

    Taylored deployment script to override default tasks since the site is currently a static site.

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,3 +42,23 @@ role :web, "broadmedium.com"                          # Your HTTP server, Apache
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+
+# Override default tasks which are not relevant to a non-rails app.
+namespace :deploy do
+  task :migrate do
+    puts "    not doing migrate because not a Rails application."
+  end
+  task :finalize_update do
+    puts "    not doing finalize_update because not a Rails application."
+  end
+  task :start do
+    puts "    not doing start because not a Rails application."
+  end
+  task :stop do 
+    puts "    not doing stop because not a Rails application."
+  end
+  task :restart do
+    puts "    not doing restart because not a Rails application."
+  end
+end
